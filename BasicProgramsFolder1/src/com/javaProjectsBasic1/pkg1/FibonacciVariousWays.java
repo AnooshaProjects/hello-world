a18d1822c115d7788a1c1d6481feca8214cb0817
Anoosha <anoosha16@gmail.com>
2024-11-08 19:15:52 -0500

    add dynamic programming approach for fibonacci + refactor code into modules for each approach

--- BasicProgramsFolder1/src/com/javaProjectsBasic1/pkg1/FibonacciVariousWays.java
+++ BasicProgramsFolder1/src/com/javaProjectsBasic1/pkg1/FibonacciVariousWays.java
@@ -5,6 +5,22 @@ import java.util.Scanner;
 public class FibonacciVariousWays {
 
     public static void main(String[] args) {
+        System.out.println("FibonacciVariousWays.main");
+
+        System.out.println("------------NormalWayFib---------------");
+        fibonacciNormalWay();
+
+        System.out.println("\n------------RecursionFib---------------");
+        fibonacciRecursiveWay();
+
+        System.out.println("\n------------DynamicProgrammingFib---------------");
+        fibonacciDynamicWay();
+
+        System.out.println("\n======Fibonacci3WaysDone=================");
+    }
+
+    public static void fibonacciNormalWay() {
+        System.out.println("Enter till which Fibonacci number you want the series?");
         Scanner sc=new Scanner(System.in);
         int fNumsTill = sc.nextInt();
         int num1=0;
@@ -17,8 +33,12 @@ public class FibonacciVariousWays {
             num1=num2;
             num2=currFNum;
         }
-        System.out.println("\n------------RecursionFib---------------");
-        int n=10;
+    }
+
+    public static void fibonacciRecursiveWay() {
+        System.out.println("How Many Fibonacci numbers in Recursive way?");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
         for (int i=0; i<n; i++) {
             int fnum = fib(i);
             System.out.print(fnum+" ");
@@ -30,5 +50,21 @@ public class FibonacciVariousWays {
             return n;
 
         return fib(n-1)+fib(n-2);
+    }
+
+
+    public static void fibonacciDynamicWay() {
+        System.out.println("How Many Fibonacci numbers in Dynamic way?");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] fibMemo = new int[n];
+
+        fibMemo[0]=0;
+        fibMemo[1]=1;
+        System.out.print(fibMemo[0]+" "+fibMemo[1]);
+        for (int i=2; i<n; i++){
+            fibMemo[i]=fibMemo[i-1]+fibMemo[i-2];
+            System.out.print(" "+fibMemo[i]);
+        }
     }
 }
