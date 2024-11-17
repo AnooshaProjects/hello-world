9d56702c75a9d04c9592dfb7464713a9b081177f
Anoosha <anoosha16@gmail.com>
2024-11-17 13:18:23 -0500

    clean1 regex

--- BasicProgramsFolder1/src/com/javaProjectsBasic1/pkg1/RegExTrials.java
+++ BasicProgramsFolder1/src/com/javaProjectsBasic1/pkg1/RegExTrials.java
@@ -5,32 +5,39 @@ import java.util.regex.Pattern;
 
 public class RegExTrials {
     public static void main(String[] args) {
+
         Pattern pattern= Pattern.compile("[a-d1-7]");
 
+        System.out.println("RegExp. = [a-d1-7]");
         Matcher matcher=pattern.matcher("a3");
-        System.out.println("Does Input String have given regex pattern match1= "+matcher.find());
+        System.out.println("a3 input string Pattern Matcher = "+matcher.find());
 
         Matcher matcher2=pattern.matcher("d1");
-        System.out.println("Does Input String have given regex pattern match2= "+matcher2.find());
+        System.out.println("d1 input string Pattern Matcher = "+matcher2.find());
 
         Matcher matcher3=pattern.matcher("e1");
-        System.out.println("Does Input String have given regex pattern match3= "+matcher3.find());
+        System.out.println("e1 input string Pattern Matcher =  "+matcher3.find());
 
         Matcher matcher4=pattern.matcher("e8");
-        System.out.println("Does Input String have given regex pattern match4= "+matcher4.find());
+        System.out.println("e8 input string Pattern Matcher = "+matcher4.find());
 
+        System.out.println("--------------------");
         Pattern pattern2= Pattern.compile("[abc]");
+        System.out.println("RegEx Pattern Compile = [abc]");
         Matcher matcher5=pattern2.matcher("ab");
-        System.out.println("Does Input String have given regex pattern match5= "+matcher5.find());
+        System.out.println("ab input string Pattern Matcher = "+matcher5.find());
 
-        System.out.println("String matches(...) result1="+("d1".matches("[a-d1-7]")));
-        System.out.println("String matches(...) result2="+("ab".matches("[abc]")));
-
-        System.out.println("String matches(...) result1="+("b2".matches("[a-d1-7]")));
-        System.out.println("String matches(...) result1="+("b2".matches("[a-d1-7]+")));
-        System.out.println("String matches(...) result1="+("bc".matches("[a-d1-7]")));
-        System.out.println("String matches(...) result1="+("a".matches("[a-d1-7]")));
-        System.out.println("String matches(...) result1="+("e".matches("[a-d1-7]")));
+        //-------------------------------------------------------------------------------
+        System.out.println("=========================================");
+        System.out.println("ab String.matches(...) [abc] result="+("ab".matches("[abc]")));
+        System.out.println("d1 String.matches(...) [a-d1-7] result="+("d1".matches("[a-d1-7]")));
+        System.out.println("b2 String.matches(...) [a-d1-7] result="+("b2".matches("[a-d1-7]")));
+        System.out.println("b2 String.matches(...) [a-d1-7]+ result="+("b2".matches("[a-d1-7]+")));
+        System.out.println("bf String.matches(...) [a-d1-7]+ result="+("bf".matches("[a-d1-7]+")));
+        System.out.println("bc String.matches(...) [a-d1-7] result="+("bc".matches("[a-d1-7]")));
+        System.out.println("a String.matches(...) [a-d1-7] result="+("a".matches("[a-d1-7]")));
+        System.out.println("e String.matches(...) [a-d1-7] result="+("e".matches("[a-d1-7]")));
+        System.out.println("=========================================");
 
     }
 }
