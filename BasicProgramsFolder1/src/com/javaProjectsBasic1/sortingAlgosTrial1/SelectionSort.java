a779a5b2de7ee63e3bc5d7583faacd89071ad621
Anoosha <anoosha16@gmail.com>
2025-02-28 23:14:32 -0500

    selection sort basic unoptimized 2 for loops nested

---
+++ BasicProgramsFolder1/src/com/javaProjectsBasic1/sortingAlgosTrial1/SelectionSort.java
@@ -0,0 +1,52 @@
+package com.javaProjectsBasic1.sortingAlgosTrial1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        //algo: 1) find the largest value of all elements and swap with right-most element.
+        //Or, find the smallest value of all elements and swap with left-most element.
+        //2) Then, repeat for each element in array.
+
+        Random rand=new Random();
+        int[] arr=new int[5];
+        int l=arr.length;
+        for (int i=0; i<l; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        //[46, 66, 7, 53, 82]
+        //int[] arr={46, 66, 7, 53, 82};
+        //[94, 40, 48, 19, 78]
+        //int[] arr={94, 40, 48, 19, 78};
+        System.out.println("Before sort:");
+        System.out.println(Arrays.toString(arr));
+
+        selectionSort2Fors(arr);
+
+        System.out.println("After sort:");
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void selectionSort2Fors(int[] arr) {
+        for(int i=0; i<arr.length-1; i++){
+            int minIndex=i;
+            int min=arr[i];
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j]<min){
+                    minIndex=j;
+                    min=arr[j];
+                }
+            }
+            swap(arr, i, minIndex);
+            //System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+    }
+}
