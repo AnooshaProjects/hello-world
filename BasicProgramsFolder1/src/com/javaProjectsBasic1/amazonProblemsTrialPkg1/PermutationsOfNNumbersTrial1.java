1ff53ea2be5d0406933682e8f336524a5d9d5b43
Anoosha <anoosha16@gmail.com>
2025-03-03 07:11:42 -0500

    Permutations Trial 2 added recursive call, which is partially working.

--- BasicProgramsFolder1/src/com/javaProjectsBasic1/amazonProblemsTrialPkg1/PermutationsOfNNumbersTrial1.java
+++ BasicProgramsFolder1/src/com/javaProjectsBasic1/amazonProblemsTrialPkg1/PermutationsOfNNumbersTrial1.java
@@ -13,20 +13,38 @@ public class PermutationsOfNNumbersTrial1 {
         inputList.add(12);
         inputList.add(13);
 
-        List<List<Integer>> finalOutputList = permutations(inputList);
+        List<List<Integer>> finalOutputList = new ArrayList<>();
+        myPermutations(finalOutputList, inputList);
         finalOutputList.forEach(PermutationsOfNNumbersTrial1::myPrint);
     }
 
-    private static List<List<Integer>> permutations(List<Integer> inputList) {
+    private static void myPermutations(List<List<Integer>> currOutList, List<Integer> inputList) {
 
         List<List<Integer>> listlist = new ArrayList<>();
-        listlist.add(inputList);
-        listlist.add(inputList);
 
+        if(inputList.size()==1) {
+            currOutList.add(inputList);
+            //return listlist;
+        } else if(inputList.size()==2) {
+            List<Integer> list2vals=new ArrayList<>();
+            list2vals.add(inputList.get(0));
+            list2vals.add(inputList.get(1));
+            List<Integer> list2valsSwapped=new ArrayList<>();
+            list2valsSwapped.add(inputList.get(1));
+            list2valsSwapped.add(inputList.get(0));
+            currOutList.add(list2vals);
+            currOutList.add(list2valsSwapped);
+            //return listlist;
+        } else {
+            for (Integer a : inputList) {
+                List<Integer> newPList = new ArrayList<>(inputList);
+                newPList.remove(a);
+                System.out.println("newPList size= "+newPList.size());
+                myPermutations(currOutList, newPList);
+                currOutList.forEach((eachPermAsList)-> eachPermAsList.add(0,a));
+            }
+        }
 
-
-
-        return listlist;
     }
 
     public static void myPrint(List<Integer> lst){
